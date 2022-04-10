133e415682c73aa6e75c9058ee84fda575c37f98
Adham Akmal Azmi <adham_92@live.com>
2022-04-10 16:36:44 +0800

    first commit

--- src/pages/sandbox/dialog-zustand.tsx
+++ src/pages/sandbox/dialog-zustand.tsx
@@ -7,66 +7,8 @@ import Button from '@/components/buttons/Button';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 
-import useDialogStore from '@/store/useDialogStore';
-
 export default function UseDialogPage() {
-  const open = useDialogStore.useOpen();
-
   const dialog = useDialog();
-  const [status, setStatus] = React.useState<string>(
-    'Click one of the dialog!'
-  );
-  console.log('status:', status);
-
-  // function openDanger() {
-  //   setStatus('Danger Dialog Opened');
-  //   dialog({
-  //     title: 'Danger!',
-  //     description: (
-  //       <>
-  //         This is a <strong className='text-red-600'>danger dialog</strong>.
-  //       </>
-  //     ),
-  //     catchOnCancel: true,
-  //     submitText: 'OH YES',
-  //     variant: 'danger',
-  //   })
-  //     .then(() => setStatus('Danger Dialog: Submitted'))
-  //     .catch(() => setStatus('Danger Dialog: Rejected'));
-  // }
-
-  // function openWarning() {
-  //   setStatus('Warning Dialog Opened');
-  //   dialog({
-  //     title: 'Warning!',
-  //     description: (
-  //       <>
-  //         This is a <strong className='text-yellow-600'>warning dialog</strong>.
-  //       </>
-  //     ),
-  //     catchOnCancel: true,
-  //     submitText: 'Okay',
-  //     variant: 'warning',
-  //   })
-  //     .then(() => setStatus('Warning Dialog: Submitted'))
-  //     .catch(() => setStatus('Warning Dialog: Rejected'));
-  // }
-
-  // function openSuccess() {
-  //   setStatus("Success Dialog Opened (this one don't catch reject)");
-  //   dialog({
-  //     title: 'Success!',
-  //     description: (
-  //       <>
-  //         This is a <strong className='text-green-600'>success dialog</strong>.
-  //       </>
-  //     ),
-  //     // Can be omitted
-  //     catchOnCancel: false,
-  //     submitText: 'Cool',
-  //     variant: 'success',
-  //   }).then(() => setStatus('Success Dialog: Submitted'));
-  // }
 
   const openSuccess = () => {
     dialog({
