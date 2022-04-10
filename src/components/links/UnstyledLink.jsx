188c4b0b4e5315cee271327dfa26057020d4443e
Adham Akmal Azmi <adham_92@live.com>
2022-04-10 17:35:00 +0800

    done converting to js

---
+++ src/components/links/UnstyledLink.jsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import * as React from 'react';
+
+import clsxm from '@/lib/clsxm';
+
+const UnstyledLink = React.forwardRef(
+  ({ children, href, openNewTab, className, nextLinkProps, ...rest }, ref) => {
+    const isNewTab =
+      openNewTab !== undefined
+        ? openNewTab
+        : href && !href.startsWith('/') && !href.startsWith('#');
+
+    if (!isNewTab) {
+      return (
+        <Link href={href} {...nextLinkProps}>
+          <a ref={ref} {...rest} className={className}>
+            {children}
+          </a>
+        </Link>
+      );
+    }
+
+    return (
+      <a
+        ref={ref}
+        target='_blank'
+        rel='noopener noreferrer'
+        href={href}
+        {...rest}
+        className={clsxm('cursor-newtab', className)}
+      >
+        {children}
+      </a>
+    );
+  }
+);
+
+export default UnstyledLink;
